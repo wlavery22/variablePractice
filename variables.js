8a10cefd424c09c3f443d1aacab33fcb481e4094
William Lavery <lavery_william@yahoo.com>
2022-12-03 17:57:57 -0500

    Add 3 boolean variables

--- variables.js
+++ variables.js
@@ -1,3 +1,6 @@
 var email = "lavery_william@yahoo.com"
 var streetAddress = "27 Maple Lane"
 var favoriteColors = "purple orange red"
+var drunk = false
+var inDebt = false
+var overTwentyone = true
