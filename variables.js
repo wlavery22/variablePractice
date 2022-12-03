765c57036dfe4d116fca066048bc3e74a5186494
William Lavery <lavery_william@yahoo.com>
2022-12-03 18:15:24 -0500

    Change 3 variables and add 3 console log statements

--- variables.js
+++ variables.js
@@ -4,3 +4,9 @@ var favoriteColors = "purple orange red"
 var drunk = false
 var inDebt = false
 var overTwentyone = true
+email = "franken_stein@yahoo.com"
+streetAddress = "27 Beech Tree Lane"
+favoriteColors = "teal lavender maroon"
+console.log(inDebt)
+console.log(email)
+console.log(drunk)
